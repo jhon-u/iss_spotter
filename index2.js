f47edfb6b4c916c875da516af9b58fdc010638aa
Jhon Useche <jhon.useche@telus.com>
2022-11-24 15:18:14 -0700

    Refactored original code to use promises.

---
+++ index2.js
@@ -0,0 +1,22 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  });
+// .catch((error) => {
+//   console.log("It didn't work: ", error.message);
+// });
+
+
+
+/*
+ * success, print out the deets!
+ */
+const printPassTimes = (passTimes) => {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    console.log(`Next pass at ${datetime} for ${pass.duration} seconds!`);
+  }
+};
