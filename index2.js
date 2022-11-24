bc774487b42c63736a6185fc8f91c4a2f334d3f7
Jhon Useche <jhon.useche@telus.com>
2022-11-24 15:20:18 -0700

    Commented out catch()

--- index2.js
+++ index2.js
@@ -3,10 +3,10 @@ const { nextISSTimesForMyLocation } = require('./iss_promised');
 nextISSTimesForMyLocation()
   .then((passTimes) => {
     printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
   });
-// .catch((error) => {
-//   console.log("It didn't work: ", error.message);
-// });
 
 
 
